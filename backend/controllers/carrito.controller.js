daa150ea3eb2070d6d1b075e6b9dcde1e5bac6fb
DavidRamirez05 <davidramirezrodriguez0507@gmail.com>
2026-02-27 17:01:13 -0500

    Update carrito.controller.js

---
+++ backend/controllers/carrito.controller.js
@@ -0,0 +1,15 @@
+/**
+ * CONTROLADOR PARA EL CARRITO DE COMPRAS
+ * Gestion de carrito
+ * Require autentication
+ */
+
+//Importamos el modelos
+const Carrito = require('../models/Carrito');
+const Producto = require('../models/Producto');
+const Categoria = require('../models/Categoria');
+const Subcategoria = require('../models/Subcategoria');
+
+/**
+ * 
+ */
