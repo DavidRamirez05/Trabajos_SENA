2e20b394cfef465ef6e859b1498491bda30b92b0
DavidRamirez05 <davidramirezrodriguez0507@gmail.com>
2026-03-10 14:51:05 -0500

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -20,7 +20,7 @@ const { generarToken } = require('../config/jwt');
  * @param {Object} res responde Express
  */
 
-const registrar = async (req, res) => {
+const register = async (req, res) => {
     try {
         const { nombre, apellido, email, password, telefono, direccion } = req.body;
 
